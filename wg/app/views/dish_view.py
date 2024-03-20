4c75226665063cbe5e159060cfbe20125edff34e
Duc Nhat Anh Nguyen <ducnhatanhnguyen@Anhs-iMac.local>
2024-03-20 17:38:38 +0100

    Gericht kann jetzt bearbeitet werden, navigation in dish_add_view und dish_detail_view.html hinzugefuegt

--- wg/app/views/dish_view.py
+++ wg/app/views/dish_view.py
@@ -6,13 +6,13 @@ from django.views.generic.detail import DetailView
 from app.models import Dish
 from wg.forms import DishForm
 
-def add_view(request):
+def edit_view(request):
     if request.method == "POST":
         form = DishForm(request.POST)
         form.save()
     else:
         form = DishForm()
-    context = {'dish_add_view_form': form}
+    context = {'dish_add_form': form}
     template = loader.get_template("dish_add_view.html")
     return HttpResponse(template.render(request=request, context=context))
 
@@ -23,18 +23,17 @@ def list_view(request):
     template = loader.get_template("dish_view.html")
     return HttpResponse(template.render(request=request, context=context))
 
-class DishDetailView(DetailView):
-    model = Dish
-    template_name = "dish_detail_view.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        return context
 
 def detail_view(request, id_dish):
     dish = get_object_or_404(Dish, pk=id_dish)
 
+    if request.method == "POST":
+        form = DishForm(request.POST, instance=dish)
+        form.save()
+    else:
+        form = DishForm(instance=dish)
+
     template = loader.get_template("dish_detail_view.html")
-    context = {'dish': dish}
+    context = {'dish_edit_form': form}
 
     return HttpResponse(template.render(request=request, context=context))
