1bba6ca1221f5ce24e6f5c3ee8f5135570563b01
Duc Nhat Anh Nguyen <nducnhatanh@icloud.com>
2024-03-23 18:22:58 +0100

    Minor fixes (nicht wichtig)

--- wg/wg/forms.py
+++ wg/wg/forms.py
@@ -7,8 +7,8 @@ from app.models import *
 class DishForm(forms.ModelForm):
     name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Gerichtname',
                                                          'class': 'form-headline'}), required=True, label='')
-    proximityDuration = forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder': 'min.', 'class': 'form-number'}), label='', max_value=999)
-    description = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Zubereitungsbeschreibung', 'class': 'form-textfield'},), label='')
+    proximityDuration = forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder': 'min.', 'class': 'form-number'}), label='', max_value=999, required=True)
+    description = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Zubereitungsbeschreibung', 'class': 'form-textfield'},), label='', required=False)
 
     class Meta:
         model = Dish
