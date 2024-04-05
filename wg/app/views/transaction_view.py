e2df0628aea809e956075cdc02e6dba15ea67047
Duc Nhat Anh Nguyen <nducnhatanh@icloud.com>
2024-04-05 18:06:04 +0200

    Bill: Transactions: Bind formset and model into the view, html improvements

--- wg/app/views/transaction_view.py
+++ wg/app/views/transaction_view.py
@@ -1,9 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.template import loader
+from django.forms import modelformset_factory
+
+from app.models import Transaction, TransactionBillEntry, Bill
+from wg.forms import TransactionForm
 
 def list_view(request):
+    transactions = Transaction.objects.all()
 
-    context = {}
-    template = loader.get_template()
+    TransactionFormSet = modelformset_factory(model=Transaction, form=TransactionForm)
+    formset = TransactionFormSet()
+
+    if request.method == "POST":
+        formset = TransactionFormSet(request.POST)
+        if formset.is_valid():
+            for form in formset:
+                form.save()
+    formset = TransactionFormSet()
+
+    context = {'formset': formset}
+    template = loader.get_template("bills/transactions.html")
     return HttpResponse(template.render(request=request, context=context))
