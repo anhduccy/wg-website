392db532fc2103ca3824e881b48c071ee5382766
Duc Nhat Anh Nguyen <nducnhatanh@icloud.com>
2024-09-21 15:50:12 +0200

    Bill PDF - Add sum pre-month comparision

--- app/views/bill_view.py
+++ app/views/bill_view.py
@@ -8,12 +8,6 @@ import dateutil.relativedelta as dateutil
 
 from app.models import Bill, TransactionBillEntry, Transaction
 
-def createBill(now, deadline, transactions):
-    if Transaction.objects.all().count() != 0:
-            new_bill = Bill.objects.create(creationDate=now, deadlineDate=deadline)
-            for transaction in transactions:
-                TransactionBillEntry.objects.create(bill=new_bill, transaction=transaction)
-
 def list_view(request):
     bills = Bill.objects.all().order_by('-deadlineDate')
 
@@ -30,17 +24,27 @@ def list_view(request):
     
     return HttpResponse(template.render(request=request, context=context))
 
-
 def pdf_view(request, id_bill=None):
     transactions = []
+    
+    #SUM THIS MONTH
     bill = Bill.objects.get(pk=id_bill)
     entries = TransactionBillEntry.objects.filter(bill=id_bill)
-    sum = 0 
+    sum = 0
     for entry in entries:
         transaction = Transaction.objects.get(pk=entry.transaction.id_transaction)
         tuple = (transaction, transaction.sum/3)
         transactions.append(tuple)
         sum += transaction.sum
+
+    #SUM LAST MONTH
+    bill_last_month = Bill.objects.filter(deadlineDate__lt=bill.deadlineDate).last()
+    sum_last_month = 0 
+    if(bill_last_month != None): 
+        entries_last_month = TransactionBillEntry.objects.filter(bill=bill_last_month.id_bill)
+        for entry in entries_last_month:
+            transaction = Transaction.objects.get(pk=entry.transaction.id_transaction)
+            sum_last_month += transaction.sum
     
     context = {
         'bill': bill,
@@ -48,6 +52,8 @@ def pdf_view(request, id_bill=None):
         'user_info': wg.secrets.UserInfo,
         'sumWG': sum,
         'sum': sum/3,
+        'sumWG_last_month': sum - sum_last_month if sum_last_month > 0 else 0,
+        'sum_last_month': sum/3 - sum_last_month/3 if sum_last_month/3 > 0 else 0,
         'next_date': bill.deadlineDate + dateutil.relativedelta(months=1)
     }
     response = wg.renderer.render_to_pdf("pdf/bill.html", context)
@@ -56,3 +62,9 @@ def pdf_view(request, id_bill=None):
         print("Bill-ERROR")
 
     return response
+
+def createBill(now, deadline, transactions):
+    if Transaction.objects.all().count() != 0:
+            new_bill = Bill.objects.create(creationDate=now, deadlineDate=deadline)
+            for transaction in transactions:
+                TransactionBillEntry.objects.create(bill=new_bill, transaction=transaction)
